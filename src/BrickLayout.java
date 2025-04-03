024a0c7acd6fc88c8014c80a64f4dc99d42f60b0
unknown <BT_2S14_34@BKTECH.LOCAL>
2025-04-03 09:48:25 -0400

    Initial commit

---
+++ src/BrickLayout.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BrickLayout {
+
+    private ArrayList<Brick> bricks;
+    private int[][] brickLayout;
+    private int cols;
+
+    public BrickLayout(String fileName, int cols, boolean dropAllBricks) {
+        this.cols = cols;
+        ArrayList<String> fileData = getFileData(fileName);
+        bricks = new ArrayList<Brick>();
+        for (String line : fileData) {
+            String[] points = line.split(",");
+            int start = Integer.parseInt(points[0]);
+            int end = Integer.parseInt(points[1]);
+            Brick b = new Brick(start, end);
+            bricks.add(b);
+        }
+        brickLayout = new int[30][cols];
+        if (dropAllBricks) {
+            while (bricks.size() != 0) {
+                doOneBrick();
+            }
+        }
+    }
+
+    public int[][] getBrickLayout() {
+        return brickLayout;
+    }
+
+    public void doOneBrick() {
+        int bottom = 29;
+        int height = 0;
+        if (bricks.size() != 0) {
+            Brick b = bricks.remove(0);
+            for (int i = b.getStart(); i<=b.getEnd(); i++){
+                if(checkBrickSpot(height, i)){
+                    height++;
+                }
+            }
+            for (int i = b.getStart(); i<=b.getEnd(); i++){
+                brickLayout[height][i] = 1;
+            }
+        }
+    }
+
+
+    public ArrayList<String> getFileData(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        return fileData;
+    }
+
+    public void printBrickLayout() {
+        for (int r = 0; r < brickLayout.length; r++) {
+            for (int c = 0; c < brickLayout[0].length; c++) {
+                System.out.print(brickLayout[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean checkBrickSpot(int r, int c) {
+        if (brickLayout[r][c] == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ src/DrawPanel.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
+import javax.swing.JPanel;
+import java.util.ArrayList;
+
+public class DrawPanel extends JPanel implements MouseListener{
+    private boolean[][] grid;
+    private BrickLayout input;
+
+
+    public DrawPanel() {
+        this.addMouseListener(this);
+        grid = new boolean[30][40];
+        set2dGrid();
+        input = new BrickLayout("src/bricks",40,false);
+    }
+
+    public void set2dGrid(){
+        grid = new boolean[30][40];
+        for (int i = 0;i< 0.3 * (grid.length * grid[0].length);i++){
+            grid[(int)(Math.random() * 30)][(int)(Math.random() * 40)] = true;
+        }
+    }
+
+    public void drawBrick() {
+        input.doOneBrick();
+        for (int i = 0; i<input.getBrickLayout().length;i++){
+            for (int j = 0; j<input.getBrickLayout()[0].length;j++){
+                if (input.getBrickLayout()[i][j] == 1){
+                    grid[i][j] = true;
+                }
+            }
+        }
+    }
+
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int num = 10;
+        int numy = 10;
+
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        for (int j = 0; j<30;j++) {
+            for (int i = 0; i < 40; i++) {
+                g.drawRect(num, numy, 20, 20);
+                if (grid[j][i] == true){
+                    g2.setColor(Color.GREEN);
+                    g2.fillRect(num,numy,20,20);
+                    g2.setColor(Color.BLACK);
+                }
+                num = num + 25;
+            }
+            numy = numy +25;
+            num = 10;
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        grid = new boolean[30][40];
+        drawBrick();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}

80f9cac8742ad3971ec632c8ff0f620483e5c52b
rayyanr08 <rayyanrahman26@gmail.com>
2025-04-21 21:12:50 -0400

    Update DrawPanel.java
    fixed time increment

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -20,70 +20,61 @@ public class DrawPanel extends JPanel implements MouseListener{
         input = new BrickLayout("src/bricks",40,false);
     }
 
-    public void set2dGrid(){
-        grid = new boolean[30][40];
-        for (int i = 0;i< 0.3 * (grid.length * grid[0].length);i++){
-            grid[(int)(Math.random() * 30)][(int)(Math.random() * 40)] = true;
-        }
-    }
-
-    public void drawBrick() {
-        input.doOneBrick();
-        for (int i = 0; i<input.getBrickLayout().length;i++){
-            for (int j = 0; j<input.getBrickLayout()[0].length;j++){
-                if (input.getBrickLayout()[i][j] == 1){
-                    grid[i][j] = true;
-                }
+    public void updateGrid() {
+        int[][] layout = input.getBrickLayout();
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 40; j++) {
+                grid[i][j] = layout[i][j] == 1;
             }
         }
     }
 
-    public void fallBrick() throws InterruptedException {
-        long startTime = System.currentTimeMillis();
-        long interval = 50;
-        long currentTime;
-        long check = 100;
-
-        while (true) {
-            currentTime = System.currentTimeMillis();
-            if (currentTime - startTime >= interval) {
-                input.doOneBrick();
-                startTime = currentTime;
-            }
-            Thread.sleep(check);
-        }
-    }
-
-
-    protected void paintComponent(Graphics g) {
+    
+     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int num = 10;
-        int numy = 10;
-
+        updateGrid();
 
         Graphics2D g2 = (Graphics2D) g;
+        int x = 10;
+        int y = 10;
 
-        for (int j = 0; j<30;j++) {
-            for (int i = 0; i < 40; i++) {
-                g.drawRect(num, numy, 20, 20);
-                if (grid[j][i] == true){
+        for (int row = 0; row < 30; row++) {
+            for (int col = 0; col < 40; col++) {
+                g.drawRect(x, y, 20, 20);
+                if (grid[row][col]) {
                     g2.setColor(Color.RED);
-                    g2.fillRect(num,numy,20,20);
+                    g2.fillRect(x, y, 20, 20);
                     g2.setColor(Color.BLACK);
                 }
-                num = num + 25;
+                x += 25;
             }
-            numy = numy +25;
-            num = 10;
+            y += 25;
+            x = 10;
         }
     }
-
     public void mouseClicked(MouseEvent e) {
-        grid = new boolean[30][40];
-        try {
-            fallBrick();
-        } catch (InterruptedException ex) {
-            System.out.println("dont work");;
+        if (!isFallingStarted && e.getButton() == MouseEvent.BUTTON1) {
+            isFallingStarted = true;
+            Thread fallingThread = new Thread(() -> {
+                long lastUpdate = System.currentTimeMillis();
+                while (true) {
+                    long currentTime = System.currentTimeMillis();
+                    if (currentTime - lastUpdate >= 99) {
+                        boolean hasMoreFalling = input.dropOneBrick();
+                        updateGrid();
+                        repaint();
+                        if (!hasMoreFalling) break;
+                        lastUpdate = currentTime;
+                    }
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException x) {
+                        x.printStackTrace();
+                    }
+                }
+            });
+
+            fallingThread.start();
         }
     }
 
